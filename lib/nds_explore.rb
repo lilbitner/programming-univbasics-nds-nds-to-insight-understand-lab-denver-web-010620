9a5fa97c19d613988455d51085e0a3aa542c6414
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-05 18:13:03 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,10 +1,11 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
-# Call the method directors_database to retrieve the NDS
+directors_database 
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
-  nil
+  pretty_print_nds(directors_database)
+	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
+  @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
 
 def print_first_directors_movie_titles
